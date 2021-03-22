2bb6ecf2ca6f1e3a14c7d46ff6255abbeea2ac70
hikaru0203 <h2.soccer.3h@i.softbank.jp>
2021-03-22 17:13:39 +0900

    commit2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,10 +14,8 @@ class CommentsController < ApplicationController
     @comments = Comment.all 
   end
   
-
-
-  private
+ private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id,prototype_id: params[:prototype_id])
-end
+  end
 end
--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :destroy, :update]
-  before_action :move_to_index, only: [:edit, :update]
-
+  before_action :set_prototype, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.all
@@ -31,30 +31,32 @@ class PrototypesController < ApplicationController
    end
 
    def update
-    @prototype = Prototype.find(params[:id])
-    @prototype.update(prototype_params)
     if @prototype.update
-      redirect_to prototype_path
+      redirect_to prototype_path(@prototype)
     else 
       render :edit 
     end
    end
 
    def destroy
-    @prototype = Prototype.find(params[:id])
     if @prototype.destroy
-      redirect_to prototypes_index_path
+      redirect_to root_path
+    else
+      redirect_to root_path
    end
   end
 
   private
+
   def prototype_params
    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :edit
-    end
-end
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @prototype.user
+  end
 end
